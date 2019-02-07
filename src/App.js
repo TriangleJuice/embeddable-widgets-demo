d5ccaf595555bf19d0301786eb509e5e71370302
Jasper Van Proeyen <trianglejuice@mac.com>
2019-02-07 15:51:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  Header,
+  Footer,
+} from '@acpaas-ui/react-components';
+/**
+ * More ACPaaS UI documentation can be found here:
+ * https://digipolisantwerp.github.io/acpaas-ui_react/
+ */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <Header />
         </header>
+        <div className="main u-wrapper">
+          <div className="u-container u-margin-top-xx u-margin-bottom-lg" role="main">
+            <div className="row">
+              <div className="col-xs-12">
+                <h1 className="u-margin-top-xl">Hello React starter kit!</h1>
+                <h2 className="h4 u-margin-top">Handy resources:</h2>
+                <ul className="a-list">
+                  <li>ACPaaS UI homepage: <a href="https://acpaas-ui.digipolis.be/home" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://acpaas-ui.digipolis.be<i className="fa fa-external-link"></i></a></li>
+                  <li>ACPaaS UI components: <a href="https://digipolisantwerp.github.io/acpaas-ui_react/" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://digipolisantwerp.github.io/acpaas-ui_react/<i className="fa fa-external-link"></i></a></li>
+                  <li>Core Branding: <a href="https://a-ui.github.io/core_branding_scss/" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://a-ui.github.io/core_branding_scss/<i className="fa fa-external-link"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer>
+          <Footer />
+        </footer>
       </div>
     );
   }
