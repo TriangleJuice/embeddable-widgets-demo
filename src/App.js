a2355564602b66f4a3f282c7e02f6ff512e19130
Joeri Sebrechts <joeri.sebrechts@digipolis.be>
2019-02-07 16:10:23 +0100

    add embeddable widget

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@ import {
   Header,
   Footer,
 } from '@acpaas-ui/react-components';
+import ReactDOM from 'react-dom';
+
 /**
  * More ACPaaS UI documentation can be found here:
  * https://digipolisantwerp.github.io/acpaas-ui_react/
  */
 
+const LocationWidget = window['auiEmbeddableWidgets'].reactComponent(
+  // url to the definition
+  "https://widgets-o.antwerpen.be/definitions/location-picker.json",
+  { React, ReactDOM }
+)
+
 class App extends Component {
+
+  onLocationChange(newLocation) { alert(newLocation.id); }
+
   render() {
     return (
       <div className="App">
-        <header>
-          <Header />
-        </header>
-        <div className="main u-wrapper">
-          <div className="u-container u-margin-top-xx u-margin-bottom-lg" role="main">
-            <div className="row">
-              <div className="col-xs-12">
-                <h1 className="u-margin-top-xl">Hello React starter kit!</h1>
-                <h2 className="h4 u-margin-top">Handy resources:</h2>
-                <ul className="a-list">
-                  <li>ACPaaS UI homepage: <a href="https://acpaas-ui.digipolis.be/home" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://acpaas-ui.digipolis.be<i className="fa fa-external-link"></i></a></li>
-                  <li>ACPaaS UI components: <a href="https://digipolisantwerp.github.io/acpaas-ui_react/" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://digipolisantwerp.github.io/acpaas-ui_react/<i className="fa fa-external-link"></i></a></li>
-                  <li>Core Branding: <a href="https://a-ui.github.io/core_branding_scss/" target="_blank" rel="noopener noreferrer" className="has-icon-right">https://a-ui.github.io/core_branding_scss/<i className="fa fa-external-link"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+      <header>
+        <Header />
+      </header>
+      <LocationWidget
+          onLocationChange={ result => this.onLocationChange(result) }
+          dimensions={{width: '600px', height: '300px'}}
+          className="location-widget"
+          />
+
         <footer>
           <Footer />
         </footer>
